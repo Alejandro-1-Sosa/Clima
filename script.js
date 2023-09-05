a4dd98ca279b5399b6ac2106875ed1261c959d91
Alejandro Sosa <sosaa9302@gmail.com>
2023-09-05 14:29:20 -0300

    initial commit

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+
 const apiKey = '75a755c5bee6d93e47a42db6672f79ce';
+
+// Mapeo de traducciones de descripciones de clima
+const descripcionTraducida = {
+  "clear sky": "cielo despejado",
+  "few clouds": "pocas nubes",
+  "scattered clouds": "nubes dispersas",
+  "broken clouds": "nubes rotas",
+  "overcast clouds": "nublado",
+  "mist": "niebla",
+  "light rain": "lluvia ligera",
+  "moderate rain": "lluvia moderada",
+  "heavy intensity rain": "lluvia intensa",
+};
 
 const weatherForm = document.getElementById("Weather-form");
 const cityInput = document.getElementById ("city-input");
@@ -13,21 +27,23 @@ weatherForm.addEventListener ('submit', (e) => {
         .then ((response) => response.json())
         .then ((data) => {
             const temperature = data.main.temp;
-            const description = data.weather[0].description;
+            const descripcionEnIngles = data.weather[0].description;
             const cityName = data.name;
+
+            // Traducir la descripción del clima
+            const descripcionEnEspanol = descripcionTraducida[descripcionEnIngles.toLowerCase()] || descripcionEnIngles;
 
             weatherInfo.innerHTML = `
             <span style="font-size: 30px; color: black; text-transform: capitalize;">
-                Weather in ${cityName}:<br> ${description}, <br>Temperature: ${temperature}°C
+                Clima en ${cityName}:<br> ${descripcionEnEspanol}, <br>Temperatura: ${temperature}°C
             </span>`;
                
         })
         .catch((error) => {
             console.error(error);
-            weatherInfo.innerHTML = 'Error fetching weather data.';
+            weatherInfo.innerHTML = 'Error al obtener datos del clima.';
         });
     }
-
 });
 
 
